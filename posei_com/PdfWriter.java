2fb39c8349be53a5ed63b98709b7e9294c4f6d44
VISHALKAVIYARASU <133678549+VISHALKAVIYARASU@users.noreply.github.com>
2023-11-26 16:48:18 +0530

    Add files via upload

---
+++ posei_com/PdfWriter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package posei_com;
+
+import com.itextpdf.text.Document;
+import java.io.FileOutputStream;
+
+/**
+ *
+ * @author lenovo
+ */
+class PdfWriter {
+
+    static void getInstance(Document doc, FileOutputStream fileOutputStream) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ posei_com/Posei_Com.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package posei_com;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Posei_Com {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
