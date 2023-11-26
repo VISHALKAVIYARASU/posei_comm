84ac9aa33f03fee0af7e23279dcecd3ce3490c2b
VISHALKAVIYARASU <133678549+VISHALKAVIYARASU@users.noreply.github.com>
2023-11-26 17:06:32 +0530

    Add files via upload

---
+++ Project/ConnectionProvider.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author lenovo
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Posei_comm","root","root");
+            return con;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+}
